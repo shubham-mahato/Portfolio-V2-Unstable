324bc24a108654e3b1e3b5dd3de6eae3457f90c1
Shubham Kumar Mahato <samshubham121@gmail.com>
2025-07-11 02:45:10 +0530

    Add Contact and Story

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Hero from './components/Hero'
-import NewHero from './components/NewHero'
-import About from './components/About'
-import NavBar from './components/NavBar'
+import React from "react";
+import Hero from "./components/Hero";
+import NewHero from "./components/NewHero";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden '>
-      <NavBar/>
-      <NewHero/>
-      <About/>
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <NavBar />
+      <NewHero />
+      <About />
+      <Story />
+      <Contact />
     </main>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
